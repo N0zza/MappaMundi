c50663401d8c7976b4d5788860d816c0943d6a9e
Jingo Spunk <30711461+JingoSpunk@users.noreply.github.com>
2023-10-19 16:27:32 -0700

    capitals

--- common/defines/borders.lua
+++ common/defines/borders.lua
@@ -1,3 +1,3 @@
 NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_TEXT_CUTOFF = {150, 250, 500}
 NDefines_Graphics.NGraphics.VICTORY_POINTS_DISTANCE_CUTOFF = {250, 500, 1000} -- At what distance VPs are hidden
---NDefines_Graphics.NGraphics.CAPITAL_ICON_CUTOFF = 1650
+NDefines_Graphics.NGraphics.CAPITAL_ICON_CUTOFF = 1500
