5c970ba4742bd0ed2a2b4b6906010f0e91093b57
Jingo Spunk <30711461+JingoSpunk@users.noreply.github.com>
2023-10-19 18:25:38 -0700

    vp update 2

--- common/defines/borders.lua
+++ common/defines/borders.lua
@@ -1,3 +1,3 @@
-NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_TEXT_CUTOFF = {150, 250, 500}
-NDefines_Graphics.NGraphics.VICTORY_POINTS_DISTANCE_CUTOFF = {250, 500, 1000} -- At what distance VPs are hidden
+NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_TEXT_CUTOFF = {75, 125, 250}
+NDefines_Graphics.NGraphics.VICTORY_POINTS_DISTANCE_CUTOFF = {125, 250, 500} -- At what distance VPs are hidden
 NDefines_Graphics.NGraphics.CAPITAL_ICON_CUTOFF = 1500
