efe39fd4067e4dff69e8f83d7b6f451726e67034
Jingo Spunk <30711461+JingoSpunk@users.noreply.github.com>
2023-11-15 23:14:52 -0700

    VP Tweaks

--- common/defines/borders.lua
+++ common/defines/borders.lua
@@ -1,5 +1,5 @@
-NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_TEXT_CUTOFF = {75, 125, 250}
-NDefines_Graphics.NGraphics.VICTORY_POINTS_DISTANCE_CUTOFF = {125, 250, 500} -- At what distance VPs are hidden
+NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_TEXT_CUTOFF = {37, 75, 125}
+NDefines_Graphics.NGraphics.VICTORY_POINTS_DISTANCE_CUTOFF = {75, 125, 250} -- At what distance VPs are hidden
 NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_AFTER = {0, 20} -- After this amount of VP the map icon becomes bigger dot.
 NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_MAX_VICTORY_POINTS_FOR_PERCENT = 30
-NDefines_Graphics.NGraphics.CAPITAL_ICON_CUTOFF = 1500
+NDefines_Graphics.NGraphics.CAPITAL_ICON_CUTOFF = 750
