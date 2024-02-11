209d9eb834a857ffd07eda402bc68e65f2aa34e7
N0zza <j_b_norris@hotmail.com>
2024-02-11 11:27:20 +1000

    Update borders.lua

--- common/defines/borders.lua
+++ common/defines/borders.lua
@@ -1,5 +1,7 @@
-NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_TEXT_CUTOFF = {37, 75, 125}
-NDefines_Graphics.NGraphics.VICTORY_POINTS_DISTANCE_CUTOFF = {75, 125, 250} -- At what distance VPs are hidden
-NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_AFTER = {0, 20} -- After this amount of VP the map icon becomes bigger dot.
-NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_MAX_VICTORY_POINTS_FOR_PERCENT = 30
-NDefines_Graphics.NGraphics.CAPITAL_ICON_CUTOFF = 750
+NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_TEXT_CUTOFF = {75, 125, 250}
+NDefines_Graphics.NGraphics.VICTORY_POINTS_DISTANCE_CUTOFF = {125, 250, 500} -- At what distance VPs are hidden
+NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_AFTER = {0, 40} -- After this amount of VP the map icon becomes bigger dot.
+NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_MAX_VICTORY_POINTS_FOR_PERCENT = 40
+NDefines_Graphics.NGraphics.CAPITAL_ICON_CUTOFF = 1000
+NDefines_Graphics.NGraphics.STATE_BORDER_FADE_NEAR = 250
+NDefines_Graphics.NGraphics.STATE_BORDER_FADE_FAR = 400
